80424304be1b45401e09aaf94bfa08bc607307ef
rémi  <remirobert33530@gmail.com>
2016-01-21 10:06:35 +1100

    add asset informations struct

--- VideoSplitFrame/VIdeoSpliter.swift
+++ VideoSplitFrame/VIdeoSpliter.swift
@@ -10,16 +10,68 @@ import Foundation
 import AVFoundation
 import UIKit
 
-public class VIdeoSpliter: NSObject {
+struct AssetInformations {
     
-    public var totalFrames: Int = 0
-    public var totalSeconds: Int = 0
+    private var _frameRate: Float!
+    var frameRate: Float! {
+        get {
+            return self._frameRate
+        }
+    }
+    private var _value:CMTimeValue!
+    var value: CMTimeValue! {
+        get {
+            return self._value
+        }
+    }
+    private var _timeScale: Int64!
+    var timeScale: Int64! {
+        get {
+            return self._timeScale
+        }
+    }
+    private var _totalSeconds: Int64!
+    var totalSeconds: Int64! {
+        get {
+            return self._totalSeconds
+        }
+    }
+    private var _totalFrames: Int64!
+    var totalFrames: Int64! {
+        get {
+            return self._totalFrames
+        }
+    }
+    private var _timeValuePerFrame: Int32!
+    var timeValuePerFrame: Int32! {
+        get {
+            return self._timeValuePerFrame
+        }
+    }
+    
+    init(asset: AVAsset, track: AVAssetTrack) {
+        self._frameRate = track.nominalFrameRate
+        self._value = asset.duration.value
+        self._timeScale = Int64(asset.duration.timescale)
+        self._totalSeconds = Int64(self.value / self.timeScale)
+        self._totalFrames = self.totalSeconds * Int64(self.frameRate)
+        self._timeValuePerFrame = asset.duration.timescale / Int32(self.frameRate)
+    }
+}
+
+public class VIdeoSpliter: NSObject {
+
     public var numberFrames: Int = 1
     public var totalFramesToCapture: Int = 0
     
-    var startInterval: CMTime = {
-        return CMTimeMakeWithSeconds(1, 990)
-    }()
+    private var _assetInformations: AssetInformations!
+    var assetInformations: AssetInformations! {
+        get {
+            return self._assetInformations
+        }
+    }
+    
+    var startInterval: CMTime?
     var endInterval: CMTime?
     
     private var urlAsset: NSURL?
@@ -39,31 +91,25 @@ public class VIdeoSpliter: NSObject {
     
     private func times(track: AVAssetTrack, asset: AVAsset, beginSecond: CMTime?, endSecond: CMTime?) -> [NSValue] {
         var times = Array<NSValue>()
-        
-        let frameRate = track.nominalFrameRate
-        let value = asset.duration.value
-        let timeScale = Int64(asset.duration.timescale)
-        let totalSeconds = Int64(value / timeScale)
-        let totalFrames = totalSeconds * Int64(frameRate)
-        
-        let timeValuePerFrame = asset.duration.timescale / Int32(frameRate)
+
+        self._assetInformations = AssetInformations(asset: asset, track: track)
         
         let beginIntervalTime = (beginSecond != nil) ? beginSecond : CMTimeMakeWithSeconds(0, asset.duration.timescale)
-        let endIntervalTime = (endSecond != nil) ? endSecond : CMTimeMakeWithSeconds(Double(totalSeconds), asset.duration.timescale)
+        let endIntervalTime = (endSecond != nil) ? endSecond : CMTimeMakeWithSeconds(Double(self.assetInformations.totalSeconds), asset.duration.timescale)
         
         if self.totalFramesToCapture > 0 {
-            if self.totalFramesToCapture > Int(totalFrames) {
+            if self.totalFramesToCapture > Int(self.assetInformations.totalFrames) {
                 self.numberFrames = 1
             }
             else {
-                self.numberFrames = Int(totalFrames) / self.totalFramesToCapture
+                self.numberFrames = Int(self.assetInformations.totalFrames) / self.totalFramesToCapture
             }
         }
         
-        for (var indexFrame = 0; indexFrame < Int(totalFrames); indexFrame++) {
+        for (var indexFrame = 0; indexFrame < Int(self.assetInformations.totalFrames); indexFrame++) {
             
             if indexFrame % self.numberFrames == 0 {
-                let timeValue = timeValuePerFrame * Int32(indexFrame)
+                let timeValue = self.assetInformations.timeValuePerFrame * Int32(indexFrame)
                 var frameTime = CMTime()
                 frameTime.value = Int64(timeValue)
                 frameTime.timescale = asset.duration.timescale
