fa9851827a6d4a2447a94ca67933f1a5907957ae
rémi  <remirobert33530@gmail.com>
2016-01-21 14:46:04 +1100

    add framework Splime

--- Splime/Splime.swift
+++ Splime/Splime.swift
@@ -10,7 +10,7 @@ import Foundation
 import AVFoundation
 import UIKit
 
-struct AssetInformations {
+public struct AssetInformations {
     
     private var _frameRate: Float!
     var frameRate: Float! {
@@ -24,20 +24,20 @@ struct AssetInformations {
             return self._value
         }
     }
-    private var _timeScale: Int64!
-    var timeScale: Int64! {
+    private var _timeScale: Int32!
+    var timeScale: Int32! {
         get {
             return self._timeScale
         }
     }
-    private var _totalSeconds: Int64!
-    var totalSeconds: Int64! {
+    private var _totalSeconds: Int32!
+    var totalSeconds: Int32! {
         get {
             return self._totalSeconds
         }
     }
-    private var _totalFrames: Int64!
-    var totalFrames: Int64! {
+    private var _totalFrames: Int32!
+    var totalFrames: Int32! {
         get {
             return self._totalFrames
         }
@@ -52,27 +52,30 @@ struct AssetInformations {
     init(asset: AVAsset, track: AVAssetTrack) {
         self._frameRate = track.nominalFrameRate
         self._value = asset.duration.value
-        self._timeScale = Int64(asset.duration.timescale)
-        self._totalSeconds = Int64(self.value / self.timeScale)
-        self._totalFrames = self.totalSeconds * Int64(self.frameRate)
+        self._timeScale = Int32(asset.duration.timescale)
+        self._totalSeconds = Int32(self.value) / self.timeScale
+        self._totalFrames = self.totalSeconds * Int32(self.frameRate)
         self._timeValuePerFrame = asset.duration.timescale / Int32(self.frameRate)
     }
 }
 
-public class VIdeoSpliter: NSObject {
-
-    public var numberFrames: Int = 1
-    public var totalFramesToCapture: Int = 0
+public class Splime: NSObject {
+    
+    private var asset: AVAsset?
+    private var track: AVAssetTrack?
+    
+    public var everyFrames: Int = 1
+    public var totalFrames: Int = 0
     
     private var _assetInformations: AssetInformations!
-    var assetInformations: AssetInformations! {
+    public var assetInformations: AssetInformations! {
         get {
             return self._assetInformations
         }
     }
     
-    var startInterval: CMTime?
-    var endInterval: CMTime?
+    public var startInterval: CMTime?
+    public var endInterval: CMTime?
     
     private var urlAsset: NSURL?
     public var url: String? {
@@ -84,31 +87,52 @@ public class VIdeoSpliter: NSObject {
             if let urlString = newValue {
                 if fileManager.fileExistsAtPath(urlString) {
                     self.urlAsset = NSURL(fileURLWithPath: urlString)
+                    self.initAssetTrackVideo()
                 }
             }
         }
     }
     
-    private func times(track: AVAssetTrack, asset: AVAsset, beginSecond: CMTime?, endSecond: CMTime?) -> [NSValue] {
+    private func getTrackVideo(asset: AVAsset) -> AVAssetTrack? {
+        var track: AVAssetTrack?
+        
+        for currentTrack in asset.tracks {
+            if currentTrack.mediaType == "vide" {
+                track = currentTrack
+            }
+        }
+        return track
+    }
+    
+    private func initAssetTrackVideo() {
+        if let urlAsset = self.urlAsset {
+            self.asset = AVAsset(URL: urlAsset)
+            if let asset = self.asset {
+                self.track = self.getTrackVideo(asset)
+            }
+        }
+    }
+    
+    private func times(track: AVAssetTrack, asset: AVAsset) -> [NSValue] {
         var times = Array<NSValue>()
-
+        
         self._assetInformations = AssetInformations(asset: asset, track: track)
         
-        let beginIntervalTime = (beginSecond != nil) ? beginSecond : CMTimeMakeWithSeconds(0, asset.duration.timescale)
-        let endIntervalTime = (endSecond != nil) ? endSecond : CMTimeMakeWithSeconds(Double(self.assetInformations.totalSeconds), asset.duration.timescale)
+        let beginIntervalTime = (self.startInterval != nil) ? self.startInterval : CMTimeMakeWithSeconds(0, asset.duration.timescale)
+        let endIntervalTime = (self.endInterval != nil) ? self.endInterval : CMTimeMakeWithSeconds(Double(self.assetInformations.totalSeconds), asset.duration.timescale)
         
-        if self.totalFramesToCapture > 0 {
-            if self.totalFramesToCapture > Int(self.assetInformations.totalFrames) {
-                self.numberFrames = 1
+        if self.totalFrames > 0 {
+            if self.totalFrames > Int(self.assetInformations.totalFrames) {
+                self.everyFrames = 1
             }
             else {
-                self.numberFrames = Int(self.assetInformations.totalFrames) / self.totalFramesToCapture
+                self.everyFrames = Int(self.assetInformations.totalFrames) / self.totalFrames
             }
         }
         
         for (var indexFrame = 0; indexFrame < Int(self.assetInformations.totalFrames); indexFrame++) {
             
-            if indexFrame % self.numberFrames == 0 {
+            if indexFrame % self.everyFrames == 0 {
                 let timeValue = self.assetInformations.timeValuePerFrame * Int32(indexFrame)
                 var frameTime = CMTime()
                 frameTime.value = Int64(timeValue)
@@ -128,37 +152,19 @@ public class VIdeoSpliter: NSObject {
         return times;
     }
     
-    private func getTrackVideo(asset: AVAsset) -> AVAssetTrack? {
-        var track: AVAssetTrack?
-        
-        for currentTrack in asset.tracks {
-            if currentTrack.mediaType == "vide" {
-                print("found video track")
-                track = currentTrack
-            }
-        }
-        return track
-    }
-    
-    public func videoFrames(completion : (images: [UIImage]) -> (), progressBlock: ((progress: Float) -> ())?) {
-        guard let urlAsset = self.urlAsset else {
-            print("error url not asset not found")
+    public func split(completion : (images: [UIImage]) -> (), progressBlock: ((progress: Float) -> ())?) {
+        guard let asset = self.asset, track = self.track else {
+            print("[Splime]: error asset / track video nil")
             return
         }
         
-        let asset = AVAsset(URL: urlAsset)
-        
-        guard let track = self.getTrackVideo(asset) else {
-            print("doesn't find video track")
-            return
-        }
         var currentIndexFrame = 0
         
         let imageGenerator = AVAssetImageGenerator(asset: asset)
         var frames = Array<UIImage>()
-        let times = self.times(track, asset: asset, beginSecond: nil, endSecond: nil)
+        let times = self.times(track, asset: asset)
         
-        imageGenerator.generateCGImagesAsynchronouslyForTimes(times) { (requestedTime: CMTime, cgImage: CGImage?, currentTime: CMTime, result: AVAssetImageGeneratorResult, error: NSError?) -> Void in
+        imageGenerator.generateCGImagesAsynchronouslyForTimes(times) { (_, cgImage: CGImage?, _, result: AVAssetImageGeneratorResult, error: NSError?) -> Void in
             if error == nil && result == .Succeeded {
                 if let cgImage = cgImage {
                     let currentImage = UIImage(CGImage: cgImage)
@@ -189,5 +195,5 @@ public class VIdeoSpliter: NSObject {
     init(url: String) {
         super.init()
         self.url = url
-    }    
+    }
 }
