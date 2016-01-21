52c875019c43950720f9d337693edc2940f56d6f
rémi  <remirobert33530@gmail.com>
2016-01-21 14:33:20 +1100

    update framework

---
+++ Splime/Splime/Splime.h
@@ -0,0 +1,19 @@
+//
+//  Splime.h
+//  Splime
+//
+//  Created by Remi Robert on 21/01/16.
+//  Copyright © 2016 Remi Robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Splime.
+FOUNDATION_EXPORT double SplimeVersionNumber;
+
+//! Project version string for Splime.
+FOUNDATION_EXPORT const unsigned char SplimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Splime/PublicHeader.h>
+
+
